32afb6947a6371431b7d82e857ef988f6f4930c7
baim <rizkikasim224@gmail.com>
2025-06-16 09:17:28 +0700

    add fitur

--- lib/services/stores/properties_api_services.g.dart
+++ lib/services/stores/properties_api_services.g.dart
@@ -14,7 +14,7 @@ class _PropertiesApiService implements PropertiesApiService {
     this.baseUrl,
     this.errorLogger,
   }) {
-    baseUrl ??= 'http://10.0.2.2:8000';
+    baseUrl ??= 'http://127.0.0.1:8000';
   }
 
   final Dio _dio;
